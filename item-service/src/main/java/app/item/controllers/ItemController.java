afc8a95cd817c3f1bafe43cb566a11781c2f84ba
Facu Mediotte <facumediotte@gmail.com>
2021-07-15 11:10:21 -0300

    Tolerancia a fallos y Latencia con Hystrix

--- item-service/src/main/java/app/item/controllers/ItemController.java
+++ item-service/src/main/java/app/item/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package app.item.controllers;
 
 import app.item.model.Item;
+import app.item.model.Producto;
 import app.item.model.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +24,22 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "alternativeMethod")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    //método alternativo por si existe un error llamando a la API de productos
+    public Item alternativeMethod(Long id, Integer cantidad) {
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return item;
     }
 }
