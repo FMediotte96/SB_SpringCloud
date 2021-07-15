47e8c5a0122c01dbc7ac275bc3e44152e69f515b
Facu Mediotte <facumediotte@gmail.com>
2021-07-15 12:40:10 -0300

    ZuulFilter Pre config

---
+++ zuul-server/src/main/java/api/zuul/filters/PreElapsedTimeFilter.java
@@ -0,0 +1,43 @@
+package api.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PreElapsedTimeFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreElapsedTimeFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    //Aca determinamos la condición de filtrado para que se ejecute el método run
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s request routing to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        Long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return null;
+    }
+}
