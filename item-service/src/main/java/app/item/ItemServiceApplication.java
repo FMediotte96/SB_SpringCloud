32d7b0c277bd692cc4dc5e5011fcda5fd1d7927b
Facu Mediotte <facumediotte@gmail.com>
2021-07-15 10:41:07 -0300

    Services as Eureka Clients

--- item-service/src/main/java/app/item/ItemServiceApplication.java
+++ item-service/src/main/java/app/item/ItemServiceApplication.java
@@ -2,10 +2,12 @@ package app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient("product-service")
+@EnableEurekaClient
+//@RibbonClient("product-service")
 @EnableFeignClients
 @SpringBootApplication
 public class ItemServiceApplication {
